dec83dad860e17d8e6a5bc18ab2344f25a9b4852
einverne <einverne@99ea01d4-d6ea-54af-c66c-ef13b8f520e1>
2013-03-30 09:38:06 +0000

    增加了Post方法

--- NewsFeed/src/com/and/netease/utils/HttpConn.java
+++ NewsFeed/src/com/and/netease/utils/HttpConn.java
@@ -5,24 +5,27 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
 
 public class HttpConn {
-	public static String JsonFromUrl(String url) {
-		String strURL = url;
+	
+	
+	public static String getJsonFromUrlGet(String url) {
 		String str = null;
 		//创建HTTPGET连接
-		HttpGet httpRequest = new HttpGet(strURL);
+		HttpGet httpRequest = new HttpGet(url);
 		try {
 			HttpResponse httpResponse = new DefaultHttpClient().execute(httpRequest);
 			//若状态码为200，OK
-			if (httpResponse.getStatusLine().getStatusCode() == 200) {
+			if (httpResponse.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
 				String string = EntityUtils.toString(httpResponse.getEntity());
 				
-				string = eregi_replace("(\\r\\n\\|\\r|\\n|\\n\\r)", "", string);
+//				string = eregi_replace("(\\r\\n\\|\\r|\\n|\\n\\r)", "", string);
 				str = string;
 			} else {
 				str ="Error Response: "+httpResponse.getStatusLine().toString();
@@ -40,7 +43,35 @@ public class HttpConn {
 		}
 		return str;
 	}
-
+	
+	public static String getJsonFromUrlPost(String url) {
+		String str = null;
+		//创建HTTPGET连接
+		HttpPost httpRequest = new HttpPost(url);
+		try {
+			HttpResponse httpResponse = new DefaultHttpClient().execute(httpRequest);
+			//若状态码为200，OK
+			if (httpResponse.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+				String string = EntityUtils.toString(httpResponse.getEntity());
+				
+//				string = eregi_replace("(\\r\\n\\|\\r|\\n|\\n\\r)", "", string);
+				str = string;
+			} else {
+				str ="Error Response: "+httpResponse.getStatusLine().toString();
+			}
+		} catch (ClientProtocolException e) {
+			// TODO: handle exception
+			str = e.getMessage().toString();
+			e.printStackTrace();
+		} catch (IOException e) {
+			str = e.getMessage().toString();
+			e.printStackTrace();
+		} catch (Exception e) {
+			str =e.getMessage().toString();
+			e.printStackTrace();
+		}
+		return str;
+	}
 	
 	
 	public static String eregi_replace(String strFrom,String strTo,String strTarget) {
