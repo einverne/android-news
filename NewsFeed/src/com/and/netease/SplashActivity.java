69802857e9e302c0e0e87a6c81765628129bc30f
einverne <einverne@99ea01d4-d6ea-54af-c66c-ef13b8f520e1>
2013-04-28 01:54:38 +0000

    Move ConnectWeb data here

--- NewsFeed/src/com/and/netease/SplashActivity.java
+++ NewsFeed/src/com/and/netease/SplashActivity.java
@@ -1,13 +1,18 @@
 package com.and.netease;
 
+import com.and.netease.utils.ConnectWeb;
+import com.and.netease.utils.DBAdapter;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 
 public class SplashActivity extends Activity {
 
-	private final int SPLASH_DISPLAY_LENGHT = 200;
+	private final int SPLASH_DISPLAY_LENGHT = 4000;
+	protected DBAdapter dbadapter;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -18,16 +23,22 @@ public class SplashActivity extends Activity {
 		 * New Handler to start the Menu-Activity and close this Splash-Screen
 		 * after some seconds.
 		 */
+		dbadapter = new DBAdapter(this);
 		new Handler().postDelayed(new Runnable() {
 			@Override
 			public void run() {
+				ConnectWeb.getzuijinxinwen(dbadapter);
+				ConnectWeb.getpeoples(dbadapter);
+				ConnectWeb.getplaces(dbadapter);
+				ConnectWeb.getdivisions(dbadapter);
 				/* Create an Intent that will start the Menu-Activity. */
 				Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
 				SplashActivity.this.startActivity(mainIntent);
 				SplashActivity.this.finish();
 			}
 		}, SPLASH_DISPLAY_LENGHT);
-
+		
+		
 //		Toast.makeText(SplashActivity.this, "......", Toast.LENGTH_LONG);
 //		
 //		TimerTask task = new TimerTask() {
